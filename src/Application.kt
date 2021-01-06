a004e4f927962824493e4e5830472e1e46c980e9
André <matthiez@gmail.com>
2021-01-06 19:40:49 +0100

    add endpoint balance

--- src/Application.kt
+++ src/Application.kt
@@ -1,8 +1,6 @@
 package com.sms77
 
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.*
@@ -10,41 +8,14 @@ import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import kotlinx.coroutines.*
 import io.ktor.client.features.logging.*
-import io.ktor.client.utils.*
-import io.ktor.features.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    val client = HttpClient(CIO) {
-        install(JsonFeature) {
-            serializer = GsonSerializer()
-        }
-        install(Logging) {
-            level = LogLevel.HEADERS
-            developmentMode = true
-/*            buildHeaders {
-                append("sentWith", "Kotlin")
-                append("Authorization", "Basic ${System.getenv("SMS77_DUMMY_API_KEY")}")
-            }*/
-        }
-/*        install(DefaultHeaders) {
-            header("sentWith", "Kotlin")
-            println("apiKey: ${System.getenv("SMS77_DUMMY_API_KEY")}")
-            header("Authorization", "Basic ${System.getenv("SMS77_DUMMY_API_KEY")}")
-        }*/
-        defaultRequest {
-            header("sentWith", "Kotlin")
-            println("apiKey: ${System.getenv("SMS77_DUMMY_API_KEY")}")
-            header("Authorization", "Basic ${System.getenv("SMS77_DUMMY_API_KEY")}")
-        }
-/*        buildHeaders {
-            append("sentWith", "Kotlin")
-            append("Authorization", "Basic ${System.getenv("SMS77_DUMMY_API_KEY")}")
-        }*/
-    }
+    val client = getClient()
+
     runBlocking {
         /*
         val message = client.post<JsonSampleClass> {
@@ -54,12 +25,31 @@ fun Application.module(testing: Boolean = false) {
         }
         */
 
-        val balance = client.get<String> {
-            url("https://gateway.sms77.io/api/balance")
-        }
-        print("balance: $balance")
+        println("balance: ${balance(client)}")
     }
 }
-class BalanceParams()
+
+suspend fun balance(client: HttpClient): Float {
+    return client.get<String> {
+        url("https://gateway.sms77.io/api/balance")
+    }.toFloat()
+}
+
+fun getClient(): HttpClient {
+    return HttpClient(CIO) {
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+        install(Logging) {
+            level = LogLevel.HEADERS
+        }
+        defaultRequest {
+            developmentMode = true
+            header("sentWith", "Kotlin")
+            header("Authorization", "Basic ${System.getenv("SMS77_DUMMY_API_KEY")}")
+        }
+    }
+}
+
 data class JsonSampleClass(val hello: String)
 
