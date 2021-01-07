fda1d517c219d4fb040ebdc4e05cb3100402c8d9
André <matthiez@gmail.com>
2021-01-07 16:21:59 +0100

    add endpoint hooks

--- src/Application.kt
+++ src/Application.kt
@@ -11,6 +11,11 @@ import io.ktor.client.request.*
 import kotlinx.coroutines.*
 import io.ktor.client.features.logging.*
 import io.ktor.http.*
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty1
+import kotlin.reflect.KType
 import kotlin.reflect.full.memberProperties
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -24,62 +29,64 @@ fun Application.module(testing: Boolean = false) {
         //println("balance: ${balance(client)}")
         //println(getContactsCsv(client))
         //println(getContactsJson(client))
+
+        println(
+            subscribeHook(
+                client, SubscribeHookParams(
+                    event_type = HookEventType.SmsInbound,
+                    request_method = HookRequestMethod.Get,
+                    target_url = "http://my.tld/kotlin/${DateTimeFormatter.ISO_INSTANT.format(Instant.now())}"
+                )
+            )
+        )
     }
 }
 
 suspend fun createContactCsv(client: HttpClient): String {
     return client.get {
-        url("https://gateway.sms77.io/api/contacts?action=write")
+        url("https://gateway.sms77.io/api/contacts?action=${ContactsAction.Write}")
     }
 }
 
 suspend fun createContactJson(client: HttpClient): CreateContactResponse {
     return client.get {
-        url("https://gateway.sms77.io/api/contacts?action=write&json=1")
+        url("https://gateway.sms77.io/api/contacts?action=${ContactsAction.Write}&json=1")
     }
 }
 
 suspend fun editContactCsv(client: HttpClient, params: EditContactParams): String {
-    var url = "https://gateway.sms77.io/api/contacts?action=write"
-
-    for (prop in EditContactParams::class.memberProperties) {
-        val value = prop.get(params)
-
-        if (null != value && false != value) {
-            url += "&${prop.name}=${value}"
-        }
-    }
-
     return client.get {
-        url(url)
+        url(
+            toQueryString(
+                "contacts?action=${ContactsAction.Write}",
+                EditContactParams::class.memberProperties,
+                params
+            )
+        )
     }
 }
 
 suspend fun editContactJson(client: HttpClient, params: EditContactParams): EditContactResponse {
-    var url = "https://gateway.sms77.io/api/contacts?action=write&json=1"
-
-    for (prop in EditContactParams::class.memberProperties) {
-        val value = prop.get(params)
-
-        if (null != value && false != value) {
-            url += "&${prop.name}=${value}"
-        }
-    }
-
     return client.get {
-        url(url)
+        url(
+            toQueryString(
+                "contacts?action=${ContactsAction.Write}&json=1",
+                EditContactParams::class.memberProperties,
+                params
+            )
+        )
     }
 }
 
 suspend fun deleteContactCsv(client: HttpClient, params: DeleteContactParams): String {
     return client.get {
-        url("https://gateway.sms77.io/api/contacts?action=del&id=${params.id}")
+        url("https://gateway.sms77.io/api/contacts?action=${ContactsAction.Delete}&id=${params.id}")
     }
 }
 
 suspend fun deleteContactJson(client: HttpClient, params: DeleteContactParams): DeleteContactResponse {
     return client.get {
-        url("https://gateway.sms77.io/api/contacts?action=del&json=1&id=${params.id}")
+        url("https://gateway.sms77.io/api/contacts?action=${ContactsAction.Delete}&json=1&id=${params.id}")
     }
 }
 
@@ -91,13 +98,37 @@ suspend fun getBalance(client: HttpClient): Float {
 
 suspend fun getContactsCsv(client: HttpClient): String {
     return client.get {
-        url("https://gateway.sms77.io/api/contacts?action=read")
+        url("https://gateway.sms77.io/api/contacts?action=${ContactsAction.Read}")
     }
 }
 
 suspend fun getContactsJson(client: HttpClient): List<Contact> {
     return client.get {
-        url("https://gateway.sms77.io/api/contacts?action=read&json=1")
+        url("https://gateway.sms77.io/api/contacts?action=${ContactsAction.Read}&json=1")
+    }
+}
+
+suspend fun getHooks(client: HttpClient): List<Hook> {
+    return client.get {
+        url("https://gateway.sms77.io/api/hooks?action=${HooksAction.Read}")
+    }
+}
+
+suspend fun subscribeHook(client: HttpClient, params: SubscribeHookParams): SubscribeHookResponse {
+    return client.post {
+        url(
+            toQueryString(
+                "hooks?action=${HooksAction.Subscribe}",
+                SubscribeHookParams::class.memberProperties,
+                params
+            )
+        )
+    }
+}
+
+suspend fun unsubscribeHook(client: HttpClient, params: UnsubscribeHookParams): UnsubscribeHookResponse {
+    return client.post {
+        url("https://gateway.sms77.io/api/hooks?action=${HooksAction.Unsubscribe}&id=${params.id}")
     }
 }
 
@@ -119,10 +150,66 @@ fun getClient(): HttpClient {
     }
 }
 
+private inline fun <reified T> toQueryString(endpoint: String, props: Collection<KProperty1<T, *>>, params: T): String {
+    var url = "https://gateway.sms77.io/api/${endpoint}"
+
+    for (prop in props) {
+        val value = prop.get(params)
+
+        if (null != value && false != value) {
+            url += "&${prop.name}=${value}"
+        }
+    }
+
+    return url
+}
+
+data class Contact(val ID: String, val Name: String, val Number: String)
+object ContactsAction {
+    const val Delete = "del"
+    const val Read = "read"
+    const val Write = "write"
+}
+
 data class CreateContactResponse(val `return`: String, val id: String)
 data class DeleteContactParams(val id: Number)
 data class DeleteContactResponse(val `return`: String)
 data class EditContactParams(val id: Number, val email: String?, val empfaenger: String?, val nick: String?)
 data class EditContactResponse(val `return`: String)
-data class Contact(val ID: String, val Name: String, val Number: String)
+data class Hook(
+    val created: String,
+    val event_type: HookEventType,
+    val id: String,
+    val request_method: HookRequestMethod,
+    val target_url: String,
+)
 
+enum class HookEventType(val value: String) {
+    SmsInbound("sms_mo"),
+    SmsStatus("dlr"),
+    VoiceStatus("voice_status")
+}
+
+enum class HookRequestMethod(val value: String) {
+    Get("GET"),
+    Post("POST"),
+}
+
+object HooksAction {
+    const val Read = "read"
+    const val Subscribe = "subscribe"
+    const val Unsubscribe = "unsubscribe"
+}
+
+class SubscribeHookParams(
+    event_type: HookEventType,
+    val target_url: String,
+    request_method: HookRequestMethod?,
+) {
+    val event_type: String = event_type.value
+    val request_method: String? = request_method?.value
+}
+
+data class SubscribeHookResponse(val success: Boolean, val id: Number?)
+data class UnsubscribeHookParams(val id: Number)
+data class UnsubscribeHookResponse(val success: Boolean)
